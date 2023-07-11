8ef93fbee959f6bb6a4deb2ab2203f160bcabc87
Eduardo Tejeda <eduardotejedadelgado@gmail.com>
2023-07-11 10:12:59 -0400

    Add spinner and add button

--- src/store/apis/albumsApi.js
+++ src/store/apis/albumsApi.js
@@ -1,13 +1,45 @@
 import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+import { Faker, faker } from '@faker-js/faker';
+
+// DEV ONLY!!!
+const pause = (duration) => {
+    return new Promise((resolve) => {
+      setTimeout(resolve, duration);
+    });
+  };
 
 const albumsApi = createApi({
     reducerPath: 'albums', 
     baseQuery: fetchBaseQuery({
         baseUrl:'http://localhost:3005/',
+        fetchFn: async (...args) => {
+            //Remove for production
+            await pause(1000);
+            return fetch(...args);
+        }
     }),
     endpoints(builder) {
         return {
+            addAlbum:builder.mutation({
+                invalidatesTags: (result, error, user) => {
+                    return [{type: 'Album', id: user.id}];
+                },
+                query: (user) => {
+                    return {
+                        url: '/albums',
+                        method: 'POST',
+                        body: {
+                            userId: user.id,
+                            title: faker.commerce.productName()
+                        }
+                    }
+
+                }
+            }),
             fetchAlbums: builder.query({
+                providesTags: (result, error, user) => {
+                    return [{type: 'Album', id: user.id}];
+                },
                 query: (user) => {
                     return {
                         url: '/albums',
@@ -25,5 +57,5 @@ const albumsApi = createApi({
 
 });
 
-export const { useFetchAlbumsQuery } = albumsApi;
+export const { useFetchAlbumsQuery, useAddAlbumMutation } = albumsApi;
 export { albumsApi };
